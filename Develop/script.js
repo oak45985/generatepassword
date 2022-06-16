e4b0db65b6d99c41a0e08809a7f3e6cf21c0c080
oak45985 <oak45985@gmail.com>
2022-06-16 17:02:59 -0500

    alt logic finalized

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,39 @@
 // Assignment code here
+
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numberList = ["1","2","3","4","5","6","7","8","9","0"]
+var spCharList = ["!","@","#","$","%","^","&","*","(",")","-","+","~"]
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+shuffle(lowerCase);
+shuffle(upperCase);
+shuffle(numberList);
+shuffle(spCharList);
+
+console.log(lowerCase.join(''));
+console.log(upperCase.join(''));
+console.log(numberList.join(''));
+console.log(spCharList.join(''));
+
+var myPw = (lowerCase.join('') + upperCase.join('') + numberList.join('') + spCharList.join(''));
+
+console.log(myPw);
+
 function getLength() {
   var pwLength = window.prompt("Please choose the length of your new password: Enter a number between 1 & 128.");
   
