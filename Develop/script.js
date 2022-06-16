cb386ee234591e2d0c54284e386db62bdc622478
oak45985 <oak45985@gmail.com>
2022-06-15 23:14:10 -0500

    some basic logic configured

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,70 @@
 // Assignment code here
+var lc = "abcdefghijklmnopqrstuvwxyz";
+var uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sc = "!@#$%^&*()+=";
+var nm = "123456790";
+
 function getLength() {
   var pwLength = window.prompt("Please choose the length of your new password: Enter a number between 1 & 128.");
-  
-  if (pwLength < "8" && pwLength > "128") {
+  // debugger;
+  while (pwLength === "" || pwLength === null) {
+    return getLength();
+  }
+  var pwLong = parseInt(pwLength);
+
+  if (pwLong < 8 || pwLong > 128) {
     window.alert("You need to pick a number between 8 & 128.");
     return getLength();
   }
-  console.log(pwLength);
+  console.log(pwLong);
 
-}
+  var lowerCase = window.confirm("Would you like to include lower case letters?");
+
+  if (lowerCase) {
+    lowerCase = lc;
+  }
+  else {
+    lowerCase = "";
+  }
+
+  var upperCase = window.confirm("Would you like to include upper case letters?");
+
+  if(upperCase) {
+    upperCase = uc;
+  }
+  else {
+    upperCase = "";
+  }
+
+  var numerals = window.confirm("Would you like to include numbers?");
+
+  if (numerals) {
+    numerals = nm;
+  }
+  else {
+    numerals = "";
+  }
+  
+  var spChar = window.confirm("Would you like to confirm special characters (such as '!', '#', '%', etc)?");
+
+  if(spChar) {
+    spChar = sc;
+  }
+  else {
+    spChar = "";
+  }
+
+  var myPw = (lowerCase + upperCase + numerals + spChar);
+
+  console.log(myPw);
+
+  function generateThatBitch() {
+    var finalPw = myPw;
+
+    return finalPw[Math.floor(Math.random() * pwLong)];
+  }
+  console.log(generateThatBitch());
+};
 
 getLength();
 
